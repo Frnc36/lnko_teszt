228049ff60f3876cbcf9b15bad03f70a89ef275c
Frnc36 <magoriferenc19@gmail.com>
2026-03-04 15:19:28 +0100

    orai munka

--- teszt/lnkoTeszt.js
+++ teszt/lnkoTeszt.js
@@ -10,7 +10,7 @@ function lnkoTeszt() {
     `a = ${a}, b = ${b}, elvárt: 0, kapott: ${eredmeny}`,
   ); // csak akkor fogja kiírni az eredmény ha hibát kapunk
 
-  /* a nem töbszörröse b-nek, de az lnko > 1 24,18 */
+  /* Az a nem töbszörröse b-nek, de az lnko > 1 24,18  */
   a = 24;
   b = 18;
   eredmeny = lnko(a, b);
@@ -66,3 +66,68 @@ function lnkoTeszt() {
 }
 
 lnkoTeszt();
+
+/* Automatizálva */
+function Teszteset() {
+  const tesztesetLista = [
+    {
+      a: 18,
+      b: 3,
+      vart: 3,
+      szoveg: "a többszöröse b-nek",
+    },
+
+    {
+      a: 24,
+      b: 18,
+      vart: 6,
+      szoveg: "Az a nem töbszörröse b-nek, de az lnko > 1 24,18 ",
+    },
+
+    {
+      a: 15,
+      b: 22,
+      vart: 1,
+      szoveg: "lnko = 1, de egyik szám sem prím, relatív prímek",
+    },
+
+    {
+      a: 2,
+      b: 3,
+      vart: 1,
+      szoveg: "két prím szám",
+    },
+
+    {
+      a: 23,
+      b: 23,
+      vart: 23,
+      szoveg: "két azonos szám",
+    },
+
+    {
+      a: -6,
+      b: -9,
+      vart: 3,
+      szoveg: "két negatív szám",
+    },
+
+    {
+      a: 18,
+      b: -3,
+      vart: 3,
+      szoveg: "kétegyik pozitív, másik negatív",
+    },
+  ];
+
+  for (let index = 0; index < tesztesetLista.length; index++) {
+    let eredmeny = lnko(tesztesetLista[index].a, tesztesetLista[index].b);
+    console.assert(
+      eredmeny === tesztesetLista[index].vart,
+      `a = ${tesztesetLista[index].a}, b = ${tesztesetLista[index].b}, elvárt: ${tesztesetLista[index].vart}, kapott: ${eredmeny}, ${tesztesetLista[index].szoveg}`,
+    );
+  }
+  console.log("Minden teszt lefutott");
+}
+
+Teszteset();
